5374d188ed8cb52dd36feb9f6706b6e872fc0b02
dayanamolina <dayanamolina2210@gmail.com>
2025-12-28 21:20:36 -0500

    tercer commit

--- build.sh
+++ build.sh
@@ -10,3 +10,13 @@ python manage.py collectstatic --no-input
 
 # Aplicar migraciones de la base de datos
 python manage.py migrate
+
+
+# ESTO CREA TU USUARIO GRATIS
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'dayanamolina@gmail.com', '123')
+    print('Usuario creado')
+EOFÑ
