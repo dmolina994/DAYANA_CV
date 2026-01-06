28dab1a15fb65193b71bf08b854f5900997aafe8
dayanamolina <dayanamolina2210@gmail.com>
2026-01-06 00:22:55 -0500

    configuraciondenombres

--- hoja_de_vida/settings.py
+++ hoja_de_vida/settings.py
@@ -6,23 +6,26 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Seguridad
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ww9j7_=ac%06&-rvo27ci!8f)0^2)+o-m8@1+i^bxys)=%l0@2')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-ww9j7_=ac%06&-rvo27ci!8f)0^2)+o-m8@1+i^bxys)=%l0@2'
+)
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-# Render Hostnames
+# Hosts
 ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-# Aplicaciones - El orden de cloudinary_storage es importante
+# Aplicaciones
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic', 
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
@@ -41,12 +44,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ProyectoHojaDeVida.urls'
+# 🔧 AQUÍ ESTABA EL ERROR
+ROOT_URLCONF = 'hoja_de_vida.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -59,9 +63,10 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ProyectoHojaDeVida.wsgi.application'
+# 🔧 Y AQUÍ TAMBIÉN
+WSGI_APPLICATION = 'hoja_de_vida.wsgi.application'
 
-# BASE DE DATOS
+# Base de datos
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
@@ -69,45 +74,46 @@ DATABASES = {
     )
 }
 
-# CONFIGURACIÓN DE ARCHIVOS ESTÁTICOS
+# Archivos estáticos
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# --- CONFIGURACIÓN DE CLOUDINARY ---
+# Cloudinary
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-# Verificamos si las variables existen para activar el almacenamiento en la nube
-if CLOUDINARY_STORAGE['CLOUD_NAME'] and CLOUDINARY_STORAGE['API_KEY'] and CLOUDINARY_STORAGE['API_SECRET']:
+if all(CLOUDINARY_STORAGE.values()):
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     MEDIA_URL = '/media/'
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 
-# Configuración de Storages para Django 4.2+
 STORAGES = {
     "default": {
-        "BACKEND": DEFAULT_FILE_STORAGE if 'DEFAULT_FILE_STORAGE' in locals() else "django.core.files.storage.FileSystemStorage",
+        "BACKEND": (
+            DEFAULT_FILE_STORAGE
+            if 'DEFAULT_FILE_STORAGE' in locals()
+            else "django.core.files.storage.FileSystemStorage"
+        ),
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
-# Idioma y Hora
+# Idioma y hora
 LANGUAGE_CODE = 'es-ec'
-TIME_ZONE = 'America/Guayaquil' 
+TIME_ZONE = 'America/Guayaquil'
 USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Permite cargar PDFs en iframes
 X_FRAME_OPTIONS = 'SAMEORIGIN'
