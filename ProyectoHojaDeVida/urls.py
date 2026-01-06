8f1b1ef3df9a1eb10b1a68fd3673114be3da6ec3
dayanamolina <dayanamolina2210@gmail.com>
2026-01-06 00:09:13 -0500

    configuracion2

--- ProyectoHojaDeVida/urls.py
+++ ProyectoHojaDeVida/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from Perfil import views
+from perfil import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
